e8f9aa0cb9ce1e53a5d6223c1830cdbc6b03e334
MariaFrancesc <mariafrancescalepore53@gmail.com>
2018-06-27 14:47:54 +0200

    Second commit

--- src/main/java/controller/BankController.java
+++ src/main/java/controller/BankController.java
@@ -11,8 +11,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.transaction.UserTransaction;
-
-import exception.*;
 import model.Account;
 import model.Customer;
 import service.BranchLocal;
